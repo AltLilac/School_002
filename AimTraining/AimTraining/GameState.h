9e1b0f5b219580eae12737feb9ad6b8466161e65
AltLilac <49546834+AltLilac@users.noreply.github.com>
2021-01-28 18:48:09 +0900

    GameStateを追加

---
+++ AimTraining/AimTraining/GameState.h
@@ -0,0 +1,14 @@
+#ifndef ___GAMESTATE
+#define ___GAMESTATE
+
+enum class State
+{
+	Title,
+	MainMenu,
+	Ranking,
+	Tutorial,
+	MainGame,
+	Result
+};
+
+#endif
---
+++ AimTraining/AimTraining/Test.cpp
@@ -0,0 +1,25 @@
+﻿#define TEST 1
+#if TEST == 1
+
+# include <Siv3D.hpp>
+
+void Main()
+{
+	// セーブデータの読み込み
+	TextReader readSaveData(U"example/SaveDate.txt");
+
+	// 読み込みに失敗したら
+	if (!readSaveData)
+	{
+		// 新規作成
+		TextWriter writeSaveData(U"SaveData.txt");
+	}
+
+
+	while (System::Update())
+	{
+		
+	}
+}
+
+#endif
