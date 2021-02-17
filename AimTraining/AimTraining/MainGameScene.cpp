b276d1a89f2c3f39a5bda8f193c6537be9b7c981
AltLilac <49546834+AltLilac@users.noreply.github.com>
2021-02-18 05:51:27 +0900

    毎フレーム的が生成され続ける不具合を修正

--- AimTraining/AimTraining/MainGameScene.cpp
+++ AimTraining/AimTraining/MainGameScene.cpp
@@ -2,22 +2,31 @@
 
 MainGameScene::MainGameScene(const InitData& init)
 	: IScene(init)
-	, timeLimit		(10)					// 制限時間の初期設定
-	, currentTime	(timeLimit)				// 現在の制限時間
-	, totalScore	(0)						// スコアの合計
-	, timeLimitSW	(true)					// 制限時間用ストップウォッチ
-	, delaySW		(false)					// 遅延処理用ストップウォッチ
-	, delayTime		(4)						// 遅延処理にかける時間
+	, timeLimit			(10)				// 制限時間の初期設定
+	, currentTime		(timeLimit)			// 現在の制限時間
+	, totalScore		(0)					// スコアの合計
+
+	, timeLimitSW		(true)				// 制限時間用ストップウォッチ
+	, delaySW			(false)				// 遅延処理用ストップウォッチ
+	, coolTimeSW		(false)				// 的のスポーン間隔用ストップウォッチ
+	, visibleDurationSW	(false)				// 的を表示し続ける時間を制御するストップウォッチ
+
+	, delayTime			(4)					// 遅延処理にかける時間
+
+	, visibleTarget		(false)				// 的の表示、非表示を制御するフラグ
+	, prepareTarget		(true)				// 的の座標を準備するフラグ
 {
-	
+	srand((unsigned)time(NULL));
 }
 
-int MainGameScene::scoreContainer = 0;
+int MainGameScene::scoreContainer = 0;		// スコア保存用変数の初期化
 
 void MainGameScene::draw() const
 {
+	// 背景色の指定
 	Scene::SetBackground(Palette::Cyan);
 
+	// 上部の UI 表示用エリア
 	Rect(0, 0, 800, 60).draw(ColorF(0.25));
 
 	// 制限時間
@@ -25,47 +34,96 @@ void MainGameScene::draw() const
 	// スコア
 	FontAsset(U"MainGameFont")(U"Score : ", totalScore).drawAt(670, 30);
 
-	// 制限時間が 0 になったら
-	if (currentTime == 0 && delaySW.isRunning())
+	// 遅延処理が発生している時に
+	if (delaySW.isRunning())
 	{
+		// 時間切れの旨を示す UI
 		FontAsset(U"GameOverFont")(U"Time Over !").drawAt(Scene::Center(), ColorF(0.25));
 	}
 }
 
 void MainGameScene::update()
 {
-	srand((unsigned)time(NULL));
-
+	// 制限時間（ 初期制限時間 - ストップウォッチの経過時間 ）
 	currentTime = timeLimit - timeLimitSW.s();
 
 	// 制限時間用ストップウォッチが稼働していたら
 	if (timeLimitSW.isRunning())
 	{
-		// x = 40, y = 100 を左上基準とした、幅 720 高さ 460 のプレイエリア範囲内に的をランダムに生成
-		// 的の大きさは最大半径 30 最小半径 10 で生成される
-		target = new Circle(40 + rand() % 720, 100 + rand() % 460, 10 + rand() % 30);
-		target->draw();
-
-		if (target->leftClicked())
+		// 的の座標を準備できる状態なら
+		if (prepareTarget)
 		{
-			totalScore += 1;
-			delete target;
+			// 新しく的の座標とサイズを準備
+			target = new Circle(40 + rand() % 720, 100 + rand() % 460, 10 + rand() % 30);
+
+			// 的の表示を有効化
+			visibleTarget = true;
+		}
+
+		// 的の表示を管理するフラグが true なら
+		if (visibleTarget)
+		{
+			// 的の座標を新しく生成しないようにする
+			prepareTarget = false;
+
+			target->draw();
+
+			// 的の表示制限時間のカウントを開始
+			visibleDurationSW.start();
+
+			// 的を左クリックしたら
+			if (target->leftClicked())
+			{
+				// スコアの加算
+				totalScore += 1;
+
+				// 表示制限時間のカウントをリセット
+				visibleDurationSW.reset();
+
+				// 的の座標を再び準備できる状態に戻す
+				prepareTarget = true;
+
+				// 的の表示を無効化
+				visibleTarget = false;
+			}
+
+			// 的の表示制限時間を超えたら
+			if (visibleDurationSW.sF() > 1.0)
+			{
+				// 表示制限時間のカウントをリセット
+				visibleDurationSW.reset();
+
+				// 的の座標を再び準備できる状態に戻す
+				prepareTarget = true;
+
+				// 的の表示を無効化
+				visibleTarget = false;
+			}
 		}
 	}
 
 	// 制限時間が 0 になったら
 	if (currentTime < 0)
 	{
+		// 一瞬だけスコアの表記が -1 になるので 0 を入れる
 		currentTime = 0;
+
+		// 制限時間時間の進行をストップ
 		timeLimitSW.pause();
+
+		// 遅延処理開始
 		delaySW.start();
 
-		scoreContainer = totalScore;	// スコアを保存
+		// スコアを保存
+		scoreContainer = totalScore;
 
-		// delaySW の 経過時間が delayTime を超えたら
+		// 遅延処理にかけたい時間を超えたら
 		if (delaySW.s() > delayTime)
 		{
+			// 遅延処理をストップ
 			delaySW.pause();
+
+			// リザルトシーンへ遷移
 			changeScene(State::Result);
 		}
 	}
--- AimTraining/AimTraining/MainGameScene.h
+++ AimTraining/AimTraining/MainGameScene.h
@@ -6,19 +6,24 @@
 class MainGameScene : public App::Scene
 {
 private:
-	const int	timeLimit;		// 制限時間
-	int			currentTime;	// 現在の制限時間
+	const int		timeLimit;			// 制限時間
+	int				currentTime;		// 現在の制限時間
 
-	mutable	int	totalScore;		// スコア
+	int				totalScore;			// スコア
 
-	Circle*		target;			// 的の描画
+	Circle*			target;				// 的の描画
 
-	Stopwatch	timeLimitSW;	// 制限時間用ストップウォッチ
-	Stopwatch	delaySW;		// 遅延処理用ストップウォッチ
+	Stopwatch		timeLimitSW;		// 制限時間用ストップウォッチ
+	Stopwatch		delaySW;			// 遅延処理用ストップウォッチ
+	Stopwatch		coolTimeSW;			// 的のスポーン間隔用ストップウォッチ
+	Stopwatch		visibleDurationSW;	// 的を表示し続ける時間を制御するストップウォッチ
 
-	const int	delayTime;		// 遅延処理にかける時間
+	const int		delayTime;			// 遅延処理にかける時間
 
-	static int	scoreContainer;	// このシーンで獲得したスコアを保存しておく
+	bool			visibleTarget;		// 的の表示、非表示を制御するフラグ
+	bool			prepareTarget;		// 的の座標を準備するフラグ
+
+	static int		scoreContainer;		// このシーンで獲得したスコアを保存しておく
 
 public:
 	MainGameScene(const InitData& init);
@@ -26,8 +31,8 @@ public:
 	void draw()   const override;
 	void update() override;
 
-	static int GetScore() noexcept { return scoreContainer; }
+	// シーンを跨いでスコアを取得する
+	[[nodiscard]] static int GetScore() noexcept { return scoreContainer; }
 };
 
 #endif
-
